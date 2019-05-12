9faea74b4668d94abb88d2f8368d7f066d77dd24
SuHyeonJeong <jun9suhyun@khu.ac.kr>
2019-05-12 11:36:22 +0900

    17141.cpp//연구소2_미완성

--- 17141.cpp
+++ 17141.cpp
@@ -8,6 +8,7 @@ const int dy[] = { -1,1,0,0 };
 const int dx[] = { 0,0,-1,1 };
 
 int N, M;
+int ans = 10000;
 int map[50][50];
 int copy_map[50][50];
 
@@ -70,27 +71,51 @@ void DFS(int idx, int cnt)
 
 	if (cnt = M)
 	{
+		for (int y = 0; y < N; y++)
+		{
+			for (int x = 0; x < N; x++)
+			{
+				copy_map[y][x] = -1;
+			}
+		}
 		for (int i = 0; i < 10; i++)
 		{
 			if (check[i])
 			{
+				copy_map[v[i].first][v[i].second];
 				q.push(make_pair(v[i].first, v[i].second));
 			}
 		}
 
 		BFS();
 
+		bool flag = true;
+		int mx=0;
 		for (int y = 0; y < N; y++)
 		{
 			for (int x = 0; x < N; x++)
 			{
-				cout << copy_map[y][x] << " ";
+				if (map[y][x] == 0)
+				{
+					flag = false;
+					break;
+				}
+				else
+				{
+					mx = max(mx, copy_map[y][x]);
+				}
 			}
-			cout << "\n";
 		}
+
+		if (flag == true)
+		{
+			ans = min(ans, mx);
+		}
+
+		
 	}
 
-	for (int i = idx; i < v.size; i++)
+	for (int i = idx; i < v.size(); i++)
 	{
 		if (check[i] == true) continue;
 		
@@ -122,6 +147,12 @@ int main()
 	
 
 	DFS(0, 0);
+
+	if (ans == 10000)
+	{
+		ans = -1;
+	}
+	cout << ans << "\n";
 	
 	
 	return 0;
